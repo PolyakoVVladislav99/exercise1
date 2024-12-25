9ab5256eb814aa77151720ba196cacdcf295ca3b
Vladislav Polyakov <whatislovepolyakov@gmail.com>
2024-12-25 11:35:25 +0300

    task 1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,34 @@
+fun main() {
+    println("Введите строку:")
+    val input = readLine() ?: ""
+
+    if (input.isEmpty()) {
+        println("Результат: ")
+        return
+    }
+
+    val result = StringBuilder()
+    var currentChar = input[0]
+    var count = 1
+
+    for (i in 1 until input.length) {
+        if (input[i] == currentChar) {
+            count++
+        } else {
+            result.append(currentChar)
+            if (count > 1) {
+                result.append(count)
+            }
+            currentChar = input[i]
+            count = 1
+        }
+    }
+
+    // Добавляем последний символ
+    result.append(currentChar)
+    if (count > 1) {
+        result.append(count)
+    }
+
+    println("Результат: $result")
+}
